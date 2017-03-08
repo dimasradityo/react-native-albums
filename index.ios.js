c9725aad38365ce50948a084e099f0fdf27adcda
Dimas <dimsrad307@gmail.com>
2017-03-08 22:16:24 +0700

    Completed header component

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+//import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+
+//create a component
+const App = () => (
+  <Header headerText={'Albums'} />
+);
+
+//render it to the device
+AppRegistry.registerComponent('albumsUdemy', () => App);
